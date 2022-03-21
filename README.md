54d1794971eb1faa2423e4f58f7b5c9bd550ef78
Manny <mannysg88@gmail.com>
2022-03-20 20:28:50 -0500

    updated README and cleaned up Javascript

--- README.md
+++ README.md
@@ -2,53 +2,13 @@
 
 [https://mannygarcia98.github.io/weather-dashboard/](https://mannygarcia98.github.io/weather-dashboard/)
 
-## Your Task
+Weather Dashboard is a web app for getting the current weather and a 5 day forecast. Once the user searches a city, it will load the weather for that city on their next visit for convenience.
 
-Third-party APIs allow developers to access their data and functionality by making requests with specific parameters to a URL. Developers are often tasked with retrieving data from another application's API and using it in the context of their own. Your challenge is to build a weather dashboard that will run in the browser and feature dynamically updated HTML and CSS.
+## Technical
 
-Use the OpenWeather One Call API (Links to an external site.) to retrieve weather data for cities. Read through the documentation for setup and usage instructions. You will use localStorage to store any persistent data.
+The site uses a combination of 2 APIs from Open Weather Map. The first (Curent Weather Data) was used to geolocate a city based on a user's input. The latitude and longitude are passed to the next API (One Call).
 
-## User Story
-
-AS A traveler
-
-I WANT to see the weather outlook for multiple cities
-
-SO THAT I can plan a trip accordingly
-
-## Acceptance Criteria
-
-GIVEN a weather dashboard with form inputs
-
-WHEN I search for a city
-
-THEN I am presented with current and future conditions for that city and that city is added to the search history
-
-WHEN I view current weather conditions for that city
-
-THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-
-WHEN I view the UV index
-
-THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-
-WHEN I view future weather conditions for that city
-
-THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
-
-WHEN I click on a city in the search history
-
-THEN I am again presented with current and future conditions for that city
-
-## Mock-up
+## Screenshot
 
 The following image shows the web application's appearance and functionality.
-![image](./images/06-server-side-apis-homework-demo.png)
-
-## Technical Acceptance Criteria: 40%
-
-Satisfies all of the above acceptance criteria plus the following:
-
-Uses the OpenWeather API to retrieve weather data
-
-Uses localStorage to store persistent data
+![image](./images/weather-dashboard-screenshot.png)
--- js/index.js
+++ js/index.js
@@ -1,66 +1,111 @@
+//Global Variables
 var cityFormEl = document.querySelector("#city-search");
-var cityInputEl = document.querySelector("#userCity");
+var cityButtonEl = document.querySelector("#city-choice");
 var momentDate = moment().format("MM/DD/YYYY");
 var currentDateEl = document.querySelector("#current-date");
-var selectedCityEl = document.querySelector("#selected-city");
-var currentTempEl = document.querySelector("#current-temp");
-var currentWindEl = document.querySelector("#current-wind");
-var currentHumidityEl = document.querySelector("#current-humidity");
-var currentUvEl = document.querySelector("#current-uv");
-
-var addDate = function () {
-  currentDateEl.textContent = "(" + momentDate + ")";
-};
-
-addDate();
-
-var getCurrentWeather = function (city) {
-  var currWeatherUrl = "https://api.openweathermap.org/data/2.5/weather?units=imperial&q=" + city + "&appid=ddcf58abee794b9b038a7f587d591c85";
-  fetch(currWeatherUrl).then(function (response) {
-    response.json().then(function (data) {
-      console.log(data);
-      displayCurrWeather(data);
-      getForecast(data);
-    });
-  });
-};
-
-var getForecast = function (currWeather) {
-  //hardcoded lat and lon to test
-  var lat = currWeather.coord.lat;
-  var lon = currWeather.coord.lon;
-  // var lat = currWeather
-  var oneCallUrl = "https://api.openweathermap.org/data/2.5/onecall?units=imperial&lat=" + lat + "&lon=" + lon + "&appid=ddcf58abee794b9b038a7f587d591c85";
-  fetch(oneCallUrl).then(function (response) {
-    response.json().then(function (data) {
-      console.log(data);
-    });
-  });
-};
-// getForecast();
-// getCurrentWeather("harlingen");
+currentDateEl.textContent = "(" + momentDate + ")";
 
 var formSubmitHandler = function (event) {
+  var cityInputEl = document.querySelector("#userCity");
+
   event.preventDefault();
-  console.log(event);
+  // console.log(event);
   var cityName = cityInputEl.value.trim();
 
   if (cityName) {
-    getCurrentWeather(cityName);
+    getLocation(cityName);
     cityInputEl.value = "";
-    cityInputEl.setAttribute("placeholder", cityName);
+    cityInputEl.setAttribute("placeholder", "");
+    localStorage.setItem("city", cityName);
   } else {
     cityInputEl.setAttribute("placeholder", "Please enter a city name.");
   }
 };
 
-var displayCurrWeather = function (currWeather) {
-  console.log(currWeather);
-  selectedCityEl.textContent = currWeather.name;
-  currentTempEl.textContent = "Temp: " + currWeather.main.temp + "°F";
-  currentWindEl.textContent = "Wind: " + currWeather.wind.speed + "MPH";
-  currentHumidityEl.textContent = "Humidity: " + currWeather.main.humidity + "%";
-  // currentUvEl.textContent = "UV Index: " + currWeather.main.;
-  addDate();
-};
 cityFormEl.addEventListener("submit", formSubmitHandler);
+
+cityButtonEl.onclick = function (event) {
+  event.preventDefault();
+  let target = event.target;
+  cityName = target.textContent;
+  localStorage.setItem("city", cityName);
+
+  getLocation(cityName);
+};
+
+var getLocation = function (city) {
+  var currWeatherUrl = "https://api.openweathermap.org/data/2.5/weather?units=imperial&q=" + city + "&appid=ddcf58abee794b9b038a7f587d591c85";
+  fetch(currWeatherUrl).then(function (response) {
+    response.json().then(function (data) {
+      var selectedCityEl = document.querySelector("#selected-city");
+      selectedCityEl.textContent = data.name;
+
+      var lat = data.coord.lat;
+      var lon = data.coord.lon;
+      getWeather(lat, lon);
+    });
+  });
+};
+
+var getWeather = function (lat, lon) {
+  var oneCallUrl = "https://api.openweathermap.org/data/2.5/onecall?units=imperial&lat=" + lat + "&lon=" + lon + "&appid=ddcf58abee794b9b038a7f587d591c85";
+  fetch(oneCallUrl).then(function (response) {
+    response.json().then(function (data) {
+      displayWeather(data);
+    });
+  });
+};
+
+var displayWeather = function (data) {
+  // console.log(data);
+  var currentTempEl = document.querySelector("#current-temp");
+  var currentWindEl = document.querySelector("#current-wind");
+  var currentHumidityEl = document.querySelector("#current-humidity");
+  var currentUvEl = document.querySelector("#current-uv");
+  var currentWeather = data.current;
+  var iconEl = document.querySelector("#icon-current");
+
+  iconEl.src = "https://openweathermap.org/img/w/" + currentWeather.weather[0].icon + ".png";
+  currentTempEl.textContent = currentWeather.temp;
+  currentWindEl.textContent = currentWeather.wind_speed;
+  currentHumidityEl.textContent = currentWeather.humidity;
+  currentUvEl.textContent = currentWeather.uvi;
+  currentUvEl.classList.remove("bg-success", "bg-warning", "bg-danger");
+  if (currentWeather.uvi < 3) {
+    currentUvEl.classList.add("bg-success");
+  } else if (currentWeather.uvi < 6) {
+    currentUvEl.classList.add("bg-warning");
+  } else {
+    currentUvEl.classList.add("bg-danger");
+  }
+  displayForecast(data);
+};
+
+var displayForecast = function (data) {
+  for (i = 1; i < 6; i++) {
+    var forecast = data.daily[i];
+    var forecastDate = moment().add(i, "d").format("MM/DD/YYYY");
+    var dateEL = document.querySelector("#day" + i);
+    var imgEl = document.querySelector("#icon-day" + i);
+    var tempEl = document.querySelector("#temp-day" + i);
+    var windEl = document.querySelector("#wind-day" + i);
+    var humidityEl = document.querySelector("#humidity-day" + i);
+    dateEL.textContent = forecastDate;
+    imgEl.src = "https://openweathermap.org/img/w/" + forecast.weather[0].icon + ".png";
+    tempEl.textContent = forecast.feels_like.day;
+    windEl.textContent = forecast.wind_speed;
+    humidityEl.textContent = forecast.humidity;
+  }
+};
+
+var getLocalStorage = function () {
+  var cityName = localStorage.getItem("city");
+  // console.log(cityName);
+  if (!cityName) {
+    return;
+  } else {
+    getLocation(cityName);
+  }
+};
+
+getLocalStorage();
